03637376fdc258b940a5ff3f402d8556a94fd9d5
dinogreenrex <=>
2018-05-17 07:59:36 +0300

    Seed with 30 records

--- news-api/database/seeds/DatabaseSeeder.php
+++ news-api/database/seeds/DatabaseSeeder.php
@@ -12,6 +12,6 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
 	    //Seed 10 records
-	    $news = factory(App\Api\News::class, 10)->create();
+	    $news = factory(App\Api\News::class, 30)->create();
     }
 }
